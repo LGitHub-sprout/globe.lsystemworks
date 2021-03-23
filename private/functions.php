ea0ae9ab624b4d12cf06cbe4d2c1bc256a0c7825
Lester Carpenter <lsysworks@gmail.com>
2021-03-22 21:49:48 -0400

    Add Error Reporting comments to functions.php

--- private/functions.php
+++ private/functions.php
@@ -1,10 +1,37 @@
 <?php
+/**
+ * Error reporting.
+ *
+ * Display and log errors:
+ * MAMP Pro:	Server > PHP > Error Handling
+ * 						Check "All errors and warnings" box under "Write"
+ *						Check "Display" box under "To"
+ *						Overrides functions below I believe bc it sets php.ini.
+ *						Toggles 'display_errors' field in php_info.php "On/Off"
+ *
+ * @link: https://stackify.com/display-php-errors/
+ * @link: https://www.php.net/manual/en/ini.list.php
+ *
+ * ini_set tries to override php.ini file (no workie for me)
+ *
+ * error_reporting( E_ALL | E_STRICT );
+ *
+ * ini_set( 'error_reporting', E_ALL );
+ * ini_set( 'display_errors', '1' );
+ * ini_set( 'log_errors', '1' );
+ * ini_set( 'display_errors', true );
+ * ini_set( 'error_log', '/private/errors.log' );
+ * display_errors = 'On';
+ */
 
-// $test = isset ( $_GET[ 'test' ] ) ? $_GET[ 'test' ] : '';
-// Test redirect: page.php?=redirect
-// if ( $test == 'redirect' ) {
-// 	redirect_to( url_for( '/staff/subjects/index.php' ) );
-// }
+/**
+ * Test redirect: page.php?=redirect
+ *
+ * $test = isset ( $_GET[ 'test' ] ) ? $_GET[ 'test' ] : '';
+ * if ( $test == 'redirect' ) {
+ * redirect_to( url_for( '/staff/subjects/index.php' ) );
+ * }
+ */
 
 // Prefix $script_path with a '/' if none provided.
 if ( ! function_exists( 'url_for' ) ) {
